d95ae47d0dd3413658ae4acd82db138399311f96
Puja Patel <ppate343@uwo.ca>
2024-07-26 15:24:01 -0400

    projects div styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import '../css/Projects.css'
 
 const Projects = () => {
   return (
-    <section id="projects">
+    <section id="projects" className='projects-section'>
       <h2>Projects</h2>
-      <p>Details about your projects...</p>
+      <div className='projects-container'>
+      <div id='portfolio' className='project'>
+        <p>Portfolio Website React.js | Github</p>
+      </div>
+      <div id='datascience' className='project'>
+        <p>Datasience Python | Github</p>
+      </div>
+      <div id='ARecipe' className='project'>
+      <p>ARecipe  Angular.ts Express.js MongoDB | Github</p>
+      </div>
+      <div id='Cashly.io' className='project'>
+      <p>Cashly.io  REact.js Express.js MongoDB RESTApi | Github</p>
+      </div>
+      </div>
     </section>
   );
 };
